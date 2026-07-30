3bca492f08845753f9107f0313da67c3947ad925
njdpro <nathanjdavis99@gmail.com>
2026-07-30 02:25:26 -0500

    Add timezone and base URL configuration
    Added a new line for BASE_URL and TIMEZONE configuration.

--- generate.js
+++ generate.js
@@ -4,6 +4,7 @@ import { execFileSync } from "node:child_process";
 import { DateTime } from "luxon";
 
 const TZ = process.env.TIMEZONE || "America/Chicago";
+
 const BASE_URL = (
 process.env.BASE_URL ||
 "https://njdpro.github.io/dailybriefing"
@@ -12,6 +13,7 @@ process.env.BASE_URL ||
 const FEED_FILE = "./rss.xml";
 const MAX_RSS_ITEMS = Number(process.env.MAX_RSS_ITEMS || 30);
 const TTS_CHUNK_BYTES = 4500;
+
 
 // -----------------------------------------------------------------------------
 // Utilities
